65e3828fbc55ad19fbff0c812891ebef988492eb
Johnny Hausman <johnny@hausmanfamily.com>
2016-11-02 11:56:46 +0700

    *fix: Menu components that link to Pages that were just deleted will no longer return an Error.

--- assets/opstools/BuildApp/controllers/page_components/menu.js
+++ assets/opstools/BuildApp/controllers/page_components/menu.js
@@ -56,10 +56,19 @@ steal(
 							// Convert object format (same arrange)
 							var pageMenu = [];
 							pageIds.forEach(function (page) {
-								pageMenu.push({
-									id: page.id,
-									value: pages.filter(function (p) { return p.id == page.id })[0].label
-								});
+								
+								// NOTE: if a page was just deleted, an existing menu might 
+								// still be trying to reference it.  Verify it still exists
+								// before trying to add it:
+								var foundPage = pages.filter(function (p) { return p.id == page.id })[0];
+								if (foundPage && foundPage.label) {
+									pageMenu.push({
+										id: page.id,
+										value: foundPage.label
+									});
+								} else {
+									console.warn('AppBuilder:Menu: tried to reference a Page['+page.id+'] that was not found.');
+								}
 							});
 
 							// Show page menu
