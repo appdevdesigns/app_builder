d4ff9aeee561f10882ab309017a84cb2bdb03435
kidlovess <stdsitsuwan2008@gmail.com>
2016-12-29 12:36:00 +0700

    Update ageequation.js

--- assets/opstools/BuildApp/controllers/data_fields/ageequation.js
+++ assets/opstools/BuildApp/controllers/data_fields/ageequation.js
@@ -34,6 +34,22 @@ steal(function () {
 		includeHeader: true,
 		description: ''
 	};
+	
+	// datepart: 'y', 'm', 'w', 'd', 'h', 'n', 's'
+	function caldateDiff(datepart, fromdate, todate){
+		  datepart = datepart.toLowerCase();	
+  		  var diff = todate - fromdate;	
+  		  var divideBy = { w:604800000, 
+                   	d:86400000, 
+                   	h:3600000, 
+                   	n:60000, 
+                   	s:1000 };	
+  
+ 	 	return Math.floor( diff/divideBy[datepart]);
+	}
+	
+
+
 
 	ageequationDataField.editDefinition = {
 		id: componentIds.editView,
