ed02c85751ae4fa63dc952a64d6bd470fd2ca44b
kidlovess <stdsitsuwan2008@gmail.com>
2017-01-10 10:58:54 +0700

    Update equation.js

--- assets/opstools/BuildApp/controllers/data_fields/equation.js
+++ assets/opstools/BuildApp/controllers/data_fields/equation.js
@@ -64,48 +64,27 @@ steal(function () {
 	}
 
 	function getDataField(type){
-		//if(type == "Date"){
-			switch(type) {
-			case "Date":
-			return {
-				editDefinition :{
-					id: componentIds.editView,
-					rows: [
-					{
-							view: "richselect",
-							id: componentIds.dateType,
-							label: "Date Type",
-							labelWidth: "110",
-							value: 'none',
-							options: [
-							{ id: 'hours', value:  "Hours" },
-							{ id: 'days', value:  "Days" },
-							{ id: 'weeks', value: "Weeks" },
-							{ id: 'years', value: "Years" },
-							]
-					},
+		return {
+			editDefinition :{
+				id: componentIds.editView,
+				rows: [
+				{
+					view: "richselect",
+					id: componentIds.dateType,
+					label: "Date Type",
+					labelWidth: "110",
+					value: 'none',
+					options: [
+					{ id: 'hours', value:  "Hours" },
+					{ id: 'days', value:  "Days" },
+					{ id: 'weeks', value: "Weeks" },
+					{ id: 'years', value: "Years" },
 					]
-				}
+				},
+				]
 			}
-					break;
 		}
-		/*else{
-			return {
-				editDefinition :{
-					id: componentIds.editView,
-					rows: [
-					{
-						view: 'template', 
-						label: 'choose a type',
-						id:'typeSettings'
-					},
-					]
-				}
-			}
-		}*/
 	}
-
-
 
 	function showSettings(type){
 		var resultType = getDataField(type);
