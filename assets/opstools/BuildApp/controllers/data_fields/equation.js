8aaceef20c3297016b6a420bdc3b14d623df8305
kidlovess <stdsitsuwan2008@gmail.com>
2017-01-19 22:42:59 +0700

    add list

--- assets/opstools/BuildApp/controllers/data_fields/equation.js
+++ assets/opstools/BuildApp/controllers/data_fields/equation.js
@@ -261,13 +261,14 @@ steal(
 	}
 
 	function addListEquation(type){
-		var listItem = EquationManager.getDescriptions();
-		console.log(listItem);
-		for(i = 0 ; i < listItem.length ; i++){
+		$$("popuplist").add({ id:webix.uid(), value: "kid"  });
+		//var listItem = EquationManager.getDescriptions();
+		//console.log(listItem);
+		/*for(i = 0 ; i < listItem.length ; i++){
 			$$("popuplist").add({ id:webix.uid(), value: listItem[i],i});
 			$$("popuplist").add({ id:webix.uid(), value: "kid"  },2);
 			console.log("addlist:"+ listItem[i]);
-		}
+		}*/
 	}
 
 
