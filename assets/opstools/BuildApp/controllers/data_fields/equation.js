c4a4c183450278aba37719040861c04b983f8fa3
kidlovess <stdsitsuwan2008@gmail.com>
2017-01-19 08:50:44 +0700

    Update equation.js

--- assets/opstools/BuildApp/controllers/data_fields/equation.js
+++ assets/opstools/BuildApp/controllers/data_fields/equation.js
@@ -114,7 +114,25 @@ steal(
 						label: "Equation",
 						labelWidth: "100",
 						id: componentIds.equation,
-	
+						
+							on: {
+						      "onItemClick": function(newv, oldv){
+							var list = webix.ui({
+							  view:"list",
+							  width:250,
+							  height:200,
+							  top: 30,
+							  left:30,
+							  template:"#title#",
+							  select:true,
+							  data:[
+							    { id:1, title:"Item 1"},
+							    { id:2, title:"Item 2"},
+							    { id:3, title:"Item 3"}
+							  ]
+							});
+								}
+						    }
 					},
 					]
 				},
