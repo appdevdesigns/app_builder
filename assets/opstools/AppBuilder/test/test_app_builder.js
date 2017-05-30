5b9d04fda43b92f2594ba72584f9f0801ac9f245
Pong <wongpratan@hotmail.com>
2017-05-30 14:13:01 +0700

    Add custom validate to date data field & Add unit tests

--- assets/opstools/AppBuilder/test/test_app_builder.js
+++ assets/opstools/AppBuilder/test/test_app_builder.js
@@ -2,3 +2,5 @@
 // Import component test cases
 import './components/ab_work_object_list_popupEditMenu'
 import './components/ab_work_object_list_newObject'
+
+import './classes/dataFields/ABFieldDate'
