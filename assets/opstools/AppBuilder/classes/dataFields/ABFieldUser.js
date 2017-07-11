6603b77bdcc1760069e91e30ce51b7655f1c68b7
James Duncan <james.duncan@cru.org>
2017-07-11 11:12:53 +0700

    [fix] Remove workaround to prevent errors because it prevented deletes, the customDisplays fix last week removes the need for this workaround.

--- assets/opstools/AppBuilder/classes/dataFields/ABFieldUser.js
+++ assets/opstools/AppBuilder/classes/dataFields/ABFieldUser.js
@@ -303,8 +303,6 @@ class ABFieldUser extends ABFieldSelectivity {
 
 			// Listen event when selectivity value updates
 			domNode.addEventListener('change', (e) => {
-				// Fixes a bug where scrolling datatable causes a change event to fire on a cell that wasn't actually changed
-				if (typeof e.added == "undefined") return false;
 				// update just this value on our current this.model
 				var values = {};
 				values[this.columnName] = this.selectivityGet(domNode);
