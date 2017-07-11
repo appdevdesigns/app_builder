1ca36def7a6328ab4a7691ff8aa21c8036353e8c
Pong <wongpratan@hotmail.com>
2017-07-11 15:36:32 +0700

    *Fix refresh source object when add new M:1 relation column

--- assets/opstools/AppBuilder/components/ab_work_object_workspace_popupNewDataField.js
+++ assets/opstools/AppBuilder/components/ab_work_object_workspace_popupNewDataField.js
@@ -335,6 +335,10 @@ export default class AB_Work_Object_Workspace_PopupNewDataField extends OP.Compo
 
                                         // refresh linked object model
                                         linkCol.object.model().refresh();
+
+                                        // refresh sourceobject model
+                                        // NOTE: M:1 relation has to refresh model after linked object's refreshed
+                                        field.object.model().refresh();
                                     });
                                 }
 
