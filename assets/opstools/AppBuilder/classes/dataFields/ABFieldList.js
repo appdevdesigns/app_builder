3e7944a2dcbb4ee2427efc1662435ce01d66fb4e
James Duncan <james.duncan@cru.org>
2017-11-13 10:30:19 +0700

    Update color options for select list.

--- assets/opstools/AppBuilder/classes/dataFields/ABFieldList.js
+++ assets/opstools/AppBuilder/classes/dataFields/ABFieldList.js
@@ -72,9 +72,11 @@ var ids = {
 };
 
 var colors = [
-	["#FF9800", "#F44336", "#E91E63"],
-	["#9C27B0", "#673AB7", "#3F51B5"],
-	["#2196F3", "#03A9F4", "#00BCD4"]
+	["#F44336", "#E91E63", "#9C27B0", "#673AB7"], 
+	["#3F51B5", "#2196F3", "#03A9F4", "#00BCD4"], 
+	["#009688", "#4CAF50", "#8BC34A", "#CDDC39"], 
+	["#FFEB3B", "#FFC107", "#FF9800", "#FF5722"], 
+	["#795548", "#9E9E9E", "#607D8B", "#000000"]
 ];
 
 // TODO : use to render selectivity to set default values
@@ -233,11 +235,11 @@ var ABFieldListComponent = new ABFieldComponent({
 							body:{
 								view:"colorboard",
 								id:"color",
-								width:100,
-								height:100,
+								width:125,
+								height:150,
 								palette:colors,
-								left:100,
-								top:100,
+								left:125,
+								top:150,
 								on: {
 									onSelect: (hex) => {
 										var vals = $$(ids.options).getItem(item);
