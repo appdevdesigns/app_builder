6c6d626cff256a32a901f0b2c2e5de4348274af1
Johnny Hausman <johnny@hausmanfamily.com>
2018-01-08 17:42:45 +0700

    [fix] only install dependencies on initial install

--- setup/setup.js
+++ setup/setup.js
@@ -19,29 +19,44 @@ var AD = require('ad-utils');
     if (process.env.NODE_ENV != 'production') {
 
 
-    	// build out our npm devdependencies so we have access to WebPack
-    	AD.spawn.command({
-           command:'npm',
-           options:['install', 
-				"babel-core",
-				"babel-loader",
-				"babel-polyfill",
-				"babel-preset-es2015", 
-				"css-loader", 
-				"extract-text-webpack-plugin", 
-				"style-loader", 
-				"uglifyjs-webpack-plugin", 
-				"webpack"
-           ],
-           shouldEcho:true
-       })
-       .fail(function(err){
-           AD.log.error('<red> appbuilder: webpack init: NPM init exited with an error</red>');
-           AD.log(err);
-       })
-       .then(function(code){
-           AD.module.setup(localOptions);
-       });
+    	var commands = [];
+
+    	// only the 1st time do we install our webpack dependencies
+    	var pathInstall = path.join(__dirname, '..', '..', '..', '.setup_install')
+    	if ( !fs.existsSync(pathInstall)) {
+
+    		commands.push({
+	            command:'npm',
+	            options:['install', 
+					"babel-core@6.24.1",
+					"babel-loader@6.4.1",
+					"babel-polyfill@6.23.0",
+					"babel-preset-es2015@6.24.1", 
+					"css-loader@0.28.1", 
+					"es6-promise@4.1.0",
+					"extract-text-webpack-plugin@2.1.0", 
+					"style-loader@0.16.1", 
+					"uglifyjs-webpack-plugin@0.4.3", 
+					"webpack@2.5.1"
+	           ],
+	           log:'<green><bold>installing:</bold> webpack dependencies </green>',
+	           shouldEcho:true
+	       	})
+    	}
+
+    	// but everytime run our webpack build:
+    	commands.push({ command:'npm', options:['run', 'build' ], log:'<green><bold>Building</bold> App Builder  </green>', shouldEcho:true });
+
+		AD.spawn.series(commands)
+		.fail(function(err){
+		    AD.log.error('<red> appbuilder: webpack testbuild: NPM exited with an error</red>');
+		    AD.log(err);
+		})
+		.then(function(code){
+
+		    AD.module.setup(localOptions);
+		});
+
 
     } else {
 
