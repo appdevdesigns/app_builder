6c2550664501f3ffc5ba669da93d708e16574f8a
Johnny Hausman <johnny@hausmanfamily.com>
2018-01-09 12:21:14 +0700

    [fix] final working version.

--- setup/setup.js
+++ setup/setup.js
@@ -21,46 +21,34 @@ var AD = require('ad-utils');
 
     	var commands = [];
 
-//// LEFT OFF HERE
-////
-// testing out our problem with loading the AppBuilder on a new install
-// attempting to install these specific packages to see if they will work
-// as expected.
-//
-// problem: .setup_install is alredy there by the time this is run the first time
-// - look for a local .setup_install, not the global one.
+    	//
+    	// AppBuilder needs to compile it's javascript before it can load.
+    	//
 
-    	// only the 1st time do we install our webpack dependencies
+    	// only the 1st time (on an install) do we install our webpack dependencies
     	var pathInstall = path.join(__dirname, '.setup_install')
     	if ( !fs.existsSync(pathInstall)) {
 
-// attempt to limit install so we speed things up:
-
+			// attempt to limit install so we speed things up:
     		commands.push({
 	            command:'npm',
 	            options:['install', 
 	            	"moment",
-					"babel-core@6.24.1",
-					"babel-loader@6.4.1",
-					"babel-polyfill@6.23.0",
-					"babel-preset-es2015@6.24.1", 
-					"css-loader@0.28.1", 
-					"es6-promise@4.1.0",
-					"extract-text-webpack-plugin@2.1.0", 
-					"style-loader@0.16.1", 
-					"uglifyjs-webpack-plugin@0.4.3", 
-					"webpack@2.5.1"
+					"babel-core",
+					"babel-loader",
+					"babel-polyfill",
+					"babel-preset-es2015", 
+					"css-loader", 
+					"es6-promise",
+					"extract-text-webpack-plugin", 
+					"style-loader", 
+					"uglifyjs-webpack-plugin", 
+					"webpack"
 	           ],
 	           log:'<green><bold>installing:</bold> webpack dependencies </green>',
 	           shouldEcho:true
 	       	})
 
-    		// commands.push({
-	     //        command:'npm',
-	     //        options:['install'],
-	     //        log:'<green><bold>installing:</bold> webpack dependencies </green>',
-	     //        shouldEcho:true
-	     //   	})
 
 	       	fs.renameSync(path.join(__dirname, '.notsetup'), path.join(__dirname, '.setup_install'));
     	}
@@ -75,6 +63,7 @@ var AD = require('ad-utils');
 		})
 		.then(function(code){
 
+			// then continue on with our .setup() run.
 		    AD.module.setup(localOptions);
 		});
 
