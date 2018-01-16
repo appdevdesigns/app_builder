3784088e171b1bcd5bf5d5164eca9331394c087e
Pong <wongpratan@hotmail.com>
2018-01-16 14:54:28 +0700

    *Fix populate user value

--- assets/opstools/AppBuilder/classes/dataFields/ABFieldUser.js
+++ assets/opstools/AppBuilder/classes/dataFields/ABFieldUser.js
@@ -516,7 +516,7 @@ class ABFieldUser extends ABFieldSelectivity {
 			// set value to selectivity
 			this.selectivitySet(domSelectivity, rowData[this.columnName], this.App);
 		} else {
-			item.setValue();
+			item.setValue(rowData[this.columnName]);
 		}
 	}
 
