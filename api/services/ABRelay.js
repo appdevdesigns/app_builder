864d5c38fe34fed3515481b70e2d78c6ede8b8ff
Johnny Hausman <johnny@hausmanfamily.com>
2018-10-02 15:58:29 +0700

    [fix] modify API to post relay users to MCC to allow it to wait longer for the connection to complete.

--- api/services/ABRelay.js
+++ api/services/ABRelay.js
@@ -64,7 +64,7 @@ module.exports = {
             headers: {
                 'authorization': sails.config.appbuilder.mcc.accessToken
             },
-            timeout:4000,   // 4s timeout to wait for a connection to the MCC
+            timeout:opt.timeout || 4000,   // 4s timeout to wait for a connection to the MCC
 time:true,  // capture timing information during communications process
 resolveWithFullResponse: true,
             json: true // Automatically stringifies the body to JSON
