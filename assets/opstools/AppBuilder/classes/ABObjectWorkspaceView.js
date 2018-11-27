d9854acf200a398a87045d5a07cc6ee21d208933
Johnny Hausman <johnny@hausmanfamily.com>
2018-11-27 15:02:35 +0700

    [add] define new ABObjectWorkspaceView settings for ABObject

---
+++ assets/opstools/AppBuilder/classes/ABObjectWorkspaceView.js
@@ -0,0 +1,67 @@
+// ABObjectWorkspaceView.js
+//
+// Manages the settings for a view in the AppBuilder Object Workspace
+
+
+export default class ABObjectWorkspaceView {
+
+	constructor(attributes, object, defaultLabel) {
+		this.defaultLabel = defaultLabel || 'default view';
+/*
+	{
+		id:uuid(),
+
+	}
+
+*/
+		this.fromObj(attributes || {});
+
+    	// multilingual fields: label
+    	OP.Multilingual.translate(this, this, ['label']);
+
+    	this.object = object;
+
+
+    	// user ids.  if has user id, then only those users can see this.
+    	// this.users = [];  
+	}
+
+
+	/**
+	 * unique key describing this View.
+	 * @return {string}
+	 */
+	static type() {
+		return 'view';
+	}
+
+
+	/**
+	 * @method fromObj
+	 * take our persisted data, and properly load it
+	 * into this object instance.
+	 * @param {json} data  the persisted data
+	 */
+	fromObj(data) {
+
+		this.id = data.id || OP.Util.uuid();
+		this.translations = data.translations || OP.Multilingual.defaultTranslations(['label'], {label:this.defaultLabel});
+
+	}
+
+
+	/**
+	 * @method toObj()
+	 * compile our current state into a {json} object
+	 * that can be persisted.
+	 */
+	toObj () {
+
+		OP.Multilingual.unTranslate(this, this, ['label']);
+		return {
+			id: this.id,
+			translations:this.translations
+		}
+	}
+}
+
---
+++ assets/opstools/AppBuilder/classes/ABObjectWorkspaceViewKanban.js
@@ -0,0 +1,72 @@
+// ABObjectWorkspaceViewKanban.js
+//
+// Manages the settings for a Data Grid View in the AppBuilder Object Workspace
+
+import ABObjectWorkspaceView from './ABObjectWorkspaceView'
+
+var defaultValues = {
+		filterConditions:[], // array of filters to apply to the data table
+		
+	};
+
+export default class ABObjectWorkspaceViewKanban extends ABObjectWorkspaceView {
+
+	constructor(attributes, object) {
+		super(attributes, object, 'kanban');
+
+/*
+	{
+		id:uuid(),
+		type:'kanban',  
+		filterConditions:[],
+	}
+
+*/
+
+
+	}
+
+
+	/**
+	 * unique key describing this View.
+	 * @return {string}
+	 */
+	static type() {
+		return 'kanban';
+	}
+
+
+	/**
+	 * @method fromObj
+	 * take our persisted data, and properly load it
+	 * into this object instance.
+	 * @param {json} data  the persisted data
+	 */
+	fromObj(data) {
+		super.fromObj(data);
+
+		for (var v in defaultValues) {
+			this[v] = data[v] || defaultValues[v];
+		}
+
+		this.type = ABObjectWorkspaceViewKanban.type();
+	}
+
+
+	/**
+	 * @method toObj()
+	 * compile our current state into a {json} object
+	 * that can be persisted.
+	 */
+	toObj () {
+		var obj = super.toObj();
+
+		for (var v in defaultValues) {
+			obj[v] = this[v];
+		}
+
+		obj.type = 'kanban';
+		return obj;
+	}
+}
+
