2f132d660a406cd506a9f06ec579e7e3e2f429e8
wongpratan <wongpratan@hotmail.com>
2019-09-04 17:55:33 +0700

    *Fix multilingual of string data field

--- assets/opstools/AppBuilder/classes/dataFields/ABFieldString.js
+++ assets/opstools/AppBuilder/classes/dataFields/ABFieldString.js
@@ -164,13 +164,6 @@ class ABFieldString extends ABField {
     	}
     	*/
 
-    	// we're responsible for setting up our specific settings:
-    	this.settings.default = values.settings.default || '';
-    	this.settings.supportMultilingual = values.settings.supportMultilingual+"" || "0";
-
-    	// text to Int:
-    	this.settings.supportMultilingual = parseInt(this.settings.supportMultilingual);
-
   	}
 
 
@@ -212,6 +205,29 @@ class ABFieldString extends ABField {
 
 
 	/**
+	 * @method fromValues()
+	 *
+	 * initialze this object with the given set of values.
+	 * @param {obj} values
+	 */
+	fromValues(values) {
+
+		super.fromValues(values);
+
+		// we're responsible for setting up our specific settings:
+		this.settings.default = values.settings.default || '';
+		this.settings.supportMultilingual = values.settings.supportMultilingual+"" || "0";
+
+		// text to Int:
+		this.settings.supportMultilingual = parseInt(this.settings.supportMultilingual);
+
+		if (this.settings.supportMultilingual)
+			OP.Multilingual.translate(this.settings, this.settings, ["default"]);
+
+	}
+
+
+	/**
 	 * @method toObj()
 	 *
 	 * properly compile the current state of this ABApplication instance
@@ -222,14 +238,15 @@ class ABFieldString extends ABField {
 	 *
 	 * @return {json}
 	 */
-	// toObj () {
+	toObj() {
 
-	// 	var obj = super.toObj();
+		var obj = super.toObj();
 
-	// 	// obj.settings = this.settings;  // <--  super.toObj()
+		if (this.settings.supportMultilingual)
+			OP.Multilingual.unTranslate(obj.settings, obj.settings, ["default"]);
 
-	// 	return obj;
-	// }
+		return obj;
+	}
 
 
 
