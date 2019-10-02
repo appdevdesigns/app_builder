b7cbc49d6e98e8c1f5716073650453d23ebbc0a8
wongpratan <wongpratan@hotmail.com>
2019-10-02 15:22:20 +0700

    Roll back

--- api/controllers/ABApplicationController.js
+++ api/controllers/ABApplicationController.js
@@ -665,13 +665,13 @@ module.exports = {
                 return new Promise((next, err) => {
 
                     ApplicationGraph
-                        .find({id: appID})
+                        .findOne(appID)
                         .then(app => {
 
                             if (!app)
                                 return next(null);
 
-                            let result = app[0].toValidJsonFormat();
+                            let result = app.toValidJsonFormat();
 
                             // Reduce data size to the live display
                             if (pageID && result.json) {
