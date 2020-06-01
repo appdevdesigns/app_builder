56955da6b172f77a0b42e6ce8d6c6f2943923558
wongpratan <wongpratan@hotmail.com>
2020-06-01 17:07:22 +0700

    *Fix read csv file

--- assets/opstools/AppBuilder/classes/platform/CSVImporter.js
+++ assets/opstools/AppBuilder/classes/platform/CSVImporter.js
@@ -78,6 +78,9 @@ module.exports = class CSVImporter {
             (dataRows || []).forEach((row) => {
                let dataCols = [];
                if (separatedBy == ",") {
+                  // NOTE: if the file contains ,, .match() can not reconize this empty string
+                  row = row.replace(/,,/g, ", ,");
+
                   // https://stackoverflow.com/questions/11456850/split-a-string-by-commas-but-ignore-commas-within-double-quotes-using-javascript#answer-11457952
                   dataCols = row.match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g);
                } else {
