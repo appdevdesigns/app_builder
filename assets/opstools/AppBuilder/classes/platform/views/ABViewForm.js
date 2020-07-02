45dd0d5a4d92b34eb4ff6ecf15c41cd81808cc64
wongpratan <wongpratan@hotmail.com>
2020-07-02 13:50:12 +0700

    +Add validator to record rules of the form widget #111

--- assets/opstools/AppBuilder/classes/platform/views/ABViewForm.js
+++ assets/opstools/AppBuilder/classes/platform/views/ABViewForm.js
@@ -1358,16 +1358,6 @@ module.exports = class ABViewForm extends ABViewFormCore {
                   reject(err);
                })
                .then((newFormVals) => {
-                  // validate for record rules
-                  let ruleValidator = obj.isValidData(newFormVals);
-                  let isUpdatedDataValid = ruleValidator.pass();
-                  if (!isUpdatedDataValid) {
-                     OP.Error.log("Updated data is invalid.", {
-                        newFormVals: newFormVals
-                     });
-                     return resolve(newFormVals);
-                  }
-
                   this.doRecordRules(newFormVals)
                      .then(() => {
                         this.doSubmitRules(newFormVals);
