14161a645686540f9415b923aeb43461fe14c554
wongpratan <wongpratan@hotmail.com>
2020-10-07 15:45:40 +0700

    *Fix connect field to generate CSV file

--- api/controllers/ABCsvController.js
+++ api/controllers/ABCsvController.js
@@ -3,8 +3,13 @@ const uuid = require("uuid/v4");
 const ABGraphApplication = require("app_builder/api/graphModels/ABApplication");
 const ABGraphDataCollection = require("app_builder/api/graphModels/ABDataview");
 
-const OUTPUT_PATH =
-   sails.config.appbuilder["csvOutputPath"] || "/var/lib/mysql-files";
+sails.config.appbuilder.csv = sails.config.appbuilder.csv || {};
+
+const CSV_GENERATE_PATH =
+   sails.config.appbuilder.csv["generatePath"] || "/var/lib/mysql-files";
+
+const CSV_OUTPUT_PATH =
+   sails.config.appbuilder.csv["outputPath"] || "/var/lib/mysql-files";
 
 /**
  * @method getCsvWidget
@@ -127,14 +132,49 @@ let generateCsv = ({ viewCsv, userData, filename, extraWhere }) => {
          .then(() => {
             let SQL;
 
+            // Clear SELECT fields
+            query.eager("").clearSelect();
+
             // Convert display data to CSV file
             obj.fields().forEach((f) => {
+               let select;
+
                switch (f.key) {
                   case "connectObject":
+                     // 1:M, 1:1 (isSource = true)
+                     if (
+                        f.settings.linkType == "one" &&
+                        f.settings.linkViaType == "many"
+                     ) {
+                        select = f.columnName;
+                     }
+                     // M:1, 1:1 (isSource = false)
+                     else if (
+                        f.settings.linkType == "many" &&
+                        f.settings.linkViaType == "one"
+                     ) {
+                        let objLink = f.datasourceLink;
+                        let fieldLink = f.fieldLink;
+                        if (objLink && fieldLink) {
+                           let sourceColumnName = f.indexField
+                              ? f.indexField.columnName
+                              : "uuid";
+                           select = `(SELECT GROUP_CONCAT(\`uuid\` SEPARATOR ',') FROM \`${objLink.tableName}\` WHERE \`${fieldLink.columnName}\` = \`${obj.tableName}\`.\`${sourceColumnName}\`)`;
+                        }
+                     }
+                     // M:N
+                     else if (
+                        f.settings.linkType == "many" &&
+                        f.settings.linkViaType == "many"
+                     ) {
+                        let joinTablename = f.joinTableName();
+                        let joinColumnNames = f.joinColumnNames();
+                        select = `(SELECT GROUP_CONCAT(\`${joinColumnNames.targetColumnName}\` SEPARATOR ',') FROM \`${joinTablename}\` WHERE ${joinColumnNames.sourceColumnName} = \`uuid\`)`;
+                     }
+
                      break;
                   case "list":
-                     query.select(
-                        knex.raw(`
+                     select = `
                         CASE
                            ${(f.settings.options || [])
                               .map((opt) => {
@@ -143,38 +183,30 @@ let generateCsv = ({ viewCsv, userData, filename, extraWhere }) => {
                               .join(" ")}
                            ELSE ""
                         END
-                     `)
-                     );
+                     `;
                      break;
                   default:
-                     query.select(knex.raw(`IFNULL(\`${f.columnName}\`, '')`));
+                     select = `IFNULL(\`${f.columnName}\`, '')`;
                      break;
                }
+
+               if (select) query.select(knex.raw(select));
             });
 
             // Header at the first line
             let SQLHeader = "";
             if (viewCsv.settings.hasHeader == true) {
-               let orderedFields = [];
-               orderedFields = orderedFields.concat(
-                  obj
-                     .fields((f) => f.key != "connectObject")
-                     .map((f) => `"${f.label}"`)
-               );
-               // Connect fields ordering will show after normal fields
-               orderedFields = orderedFields.concat(
-                  obj
-                     .fields((f) => f.key == "connectObject")
-                     .map((f) => `"${f.label}"`)
-               );
-
                // SELECT "One", "Two", "Three", "Four", "Five", "Six" UNION ALL
-               SQLHeader = `SELECT ${orderedFields.join(",")} UNION ALL`;
+               SQLHeader = `SELECT ${obj
+                  .fields()
+                  .map((f) => `"${f.label}"`)
+                  .join(",")} UNION ALL`;
             }
 
             try {
-               SQL = `${SQLHeader} ${query.toString()}
-            INTO OUTFILE '/var/lib/mysql-files/${filename}.csv'
+               // SQL = `${SQLHeader} ${query.toString()}
+               SQL = `${SQLHeader} ${query.debug()}
+            INTO OUTFILE '${CSV_GENERATE_PATH}/${filename}.csv'
             FIELDS TERMINATED BY ','
             ENCLOSED BY '"'
             ESCAPED BY ''
@@ -212,7 +244,7 @@ let ABCsvController = {
             });
          })
          .then(() => {
-            let outputFile = `${OUTPUT_PATH}/${filename}.csv`;
+            let outputFile = `${CSV_OUTPUT_PATH}/${filename}.csv`;
 
             // check exists CSV file
             fs.access(outputFile, fs.constants.F_OK, (err) => {
@@ -228,7 +260,7 @@ let ABCsvController = {
                );
 
                // stream file to response
-               fs.createReadStream(`${OUTPUT_PATH}/${filename}.csv`)
+               fs.createReadStream(`${CSV_OUTPUT_PATH}/${filename}.csv`)
                   .on("error", function(err) {
                      return res.AD.error(err, 500);
                   })
