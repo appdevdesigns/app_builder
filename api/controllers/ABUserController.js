fcaa326872878fef716324ab8b76caa28675f0b2
wongpratan <wongpratan@hotmail.com>
2020-10-27 14:41:53 +0700

    *Fix real time update in data collection when has scope filter #200

--- api/controllers/ABUserController.js
+++ api/controllers/ABUserController.js
@@ -4,7 +4,6 @@
  * @description :: Server-side logic for managing Abobjects
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
 module.exports = {
    // REST API: /app_builder/abuser
    _config: {
@@ -57,26 +56,68 @@ module.exports = {
    getRoleScopes: function(req, res) {
       let username = req.param("user");
 
-      const ROLE_OBJECT_ID = ABSystemObject.getObjectRoleId();
-      const RoleModel = ABObjectCache.get(ROLE_OBJECT_ID);
-
-      let cond = {
-         where: {
-            glue: "and",
-            rules: [
-               {
-                  key: "users",
-                  rule: "contains",
-                  value: username
-               }
-            ]
-         }
-      };
-
-      RoleModel.queryFind(cond, req.user.data)
+      getRoles({ username, userdata: req.user.data })
          .catch(res.AD.error)
          .then((roles) => {
             res.AD.success(roles || []);
          });
+   },
+
+   // GET: /app_builder/user/myscopes
+   getMyScopes: function(req, res) {
+      const ROLE_OBJECT_ID = ABSystemObject.getObjectRoleId();
+      const SCOPE_OBJECT_ID = ABSystemObject.getObjectScopeId();
+      const RoleModel = ABObjectCache.get(ROLE_OBJECT_ID);
+      const ScopeModel = ABObjectCache.get(SCOPE_OBJECT_ID);
+
+      let username = req.user.data.username;
+
+      Promise.resolve()
+         .then(() => getRoles({ username, userdata: req.user.data }))
+         .then((roles) => {
+            let connectedField = ScopeModel.fields(
+               (f) => (f.settings || {}).linkObject == ROLE_OBJECT_ID
+            )[0];
+
+            return ScopeModel.queryFind(
+               {
+                  where: {
+                     glue: "and",
+                     rules: [
+                        {
+                           key: connectedField.id,
+                           rule: "contains",
+                           value: (roles || []).map((r) => r[RoleModel.PK()])
+                        }
+                     ]
+                  }
+               },
+               req.user.data
+            );
+         })
+         .then((scopes) => {
+            res.AD.success(scopes || []);
+         })
+         .catch(res.AD.error);
    }
 };
+
+function getRoles({ username, userdata }) {
+   const ROLE_OBJECT_ID = ABSystemObject.getObjectRoleId();
+   const RoleModel = ABObjectCache.get(ROLE_OBJECT_ID);
+
+   let cond = {
+      where: {
+         glue: "and",
+         rules: [
+            {
+               key: "users",
+               rule: "contains",
+               value: username
+            }
+         ]
+      }
+   };
+
+   return RoleModel.queryFind(cond, userdata);
+}
