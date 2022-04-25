0ad7b4865862eb0b47c221dee4f7820dee2a685c
James Duncan <jduncandesign@gmail.com>
2022-04-25 14:19:41 +0700

    update multiplier and divider to fix sums
    since we needed values to two decimals we needed to multiply numbers to ensure they came out to nice clean integers ending in 0 so we could do some simple addition.

--- api/controllers/LocalIncomeExpenseController.js
+++ api/controllers/LocalIncomeExpenseController.js
@@ -167,7 +167,7 @@ module.exports = {
       function categorySum(category, balances) {
          const filtered = balances.filter(bal => accountInCategory(bal['COA Num'], category));
          if (filtered.length > 0 ) {
-            return filtered.map(i=>i['Running Balance']).reduce((a,b)=>(((100*a)+(100*b))/100));
+            return filtered.map(i=>i['Running Balance']).reduce((a,b)=>(((1000*a)+(1000*b))/1000));
          } else {
             return 0;
          }
