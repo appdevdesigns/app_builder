cf397d094cd993fa658f5dc9016a447d4919a7e1
wongpraatan <wongpratan@hotmail.com>
2022-05-03 16:55:07 +0700

    *Update the balance sheet report data and format - https://github.com/digi-serve/planning/issues/68

--- api/controllers/ABReportBalanceController.js
+++ api/controllers/ABReportBalanceController.js
@@ -4,6 +4,10 @@ const OBJECT_IDS = {
    BALANCE: "bb9aaf02-3265-4b8c-9d9a-c0b447c2d804"
 };
 
+const QUERY_IDS = {
+   RC: "241a977c-7748-420d-9dcb-eff53e66a43f"
+};
+
 const ACCOUNT_CATEGORIES = {
    Assets: "1585806356532",
    Liabilities: "1585806356570",
@@ -19,8 +23,10 @@ const ITEM_TYPES = {
    TotalTertiary: "tertiary-total"
 };
 
-function GetViewData(fyMonth) {
+function GetViewData(rc, fyMonth) {
    return {
+      rc: rc,
+      rcOptions: [],
       fyPeriod: fyMonth,
       fyOptions: [],
       items: [
@@ -29,80 +35,46 @@ function GetViewData(fyMonth) {
             type: ITEM_TYPES.Header
          },
          {
-            title: "Current Assets",
+            id: 11,
+            title: "11000 - Banks and Cash",
+            type: ITEM_TYPES.TotalTertiary,
+            value: 0
+         },
+         {
+            title: "Other Assets",
             type: ITEM_TYPES.Header
          },
          {
-            id: 101,
-            title: "1010 - Cash On Hand",
-            value: 0
-         },
-         {
-            id: 103,
-            title: "1030 - RMB Bank",
-            value: 0
-         },
-         {
-            id: 104,
-            title: "1040 - USD Bank",
-            value: 0
-         },
-         {
-            id: "Sum10",
-            title: "Total Cash",
-            type: ITEM_TYPES.TotalTertiary,
-            value: (items) => {
-               let val101 = items.find((x) => x.id == 101).value || 0;
-               let val103 = items.find((x) => x.id == 103).value || 0;
-               let val104 = items.find((x) => x.id == 104).value || 0;
-
-               return val101 + val103 + val104;
-            }
-         },
-         {
-            id: 120,
-            title: "1200 - SheBao Bank",
-            value: 0
-         },
-         {
-            id: 121,
-            title: "1210 - Other Receivables",
-            value: 0
-         },
-         {
-            id: 13,
-            title: "1300 - Materials for sale",
+            id: 12,
+            title: "1200 - Receivables",
             value: 0
          },
          {
             id: 14,
-            title: "1400 - Other current assets",
+            title: "1400 - Products for Sale",
+            value: 0
+         },
+         {
+            id: 15,
+            title: "1500 - Advances and other current assets",
+            value: 0
+         },
+         {
+            id: 16,
+            title: "1600 - Investments",
             value: 0
          },
          {
             id: "Sum12",
-            title: "Total",
+            title: "Total Other Asset",
             type: ITEM_TYPES.TotalTertiary,
             value: (items) => {
-               let val120 = items.find((x) => x.id == 120).value || 0;
-               let val121 = items.find((x) => x.id == 121).value || 0;
-               let val13 = items.find((x) => x.id == 13).value || 0;
+               let val12 = items.find((x) => x.id == 12).value || 0;
                let val14 = items.find((x) => x.id == 14).value || 0;
+               let val15 = items.find((x) => x.id == 15).value || 0;
+               let val16 = items.find((x) => x.id == 16).value || 0;
 
-               return val120 + val121 + val13 + val14;
-            }
-         },
-         {
-            id: "SumCurrentAsset",
-            title: "Total current assets",
-            type: ITEM_TYPES.TotalSecondary,
-            value: (items) => {
-               let valSum10 =
-                  items.find((x) => x.id == "Sum10").value(items) || 0;
-               let valSum12 =
-                  items.find((x) => x.id == "Sum12").value(items) || 0;
-
-               return valSum10 + valSum12;
+               return val12 + val14 + val15 + val16;
             }
          },
          {
@@ -110,131 +82,130 @@ function GetViewData(fyMonth) {
             type: ITEM_TYPES.Header
          },
          {
-            id: 15,
-            title: "1500 - Land",
-            value: 0
-         },
-         {
-            id: 16,
-            title: "1600 - Buildings",
-            value: 0
-         },
-         {
             id: 17,
-            title: "1700 - Equipment, furniture, vehicles",
+            title: "1700 - Fixed assets and depreciation",
+            type: ITEM_TYPES.TotalTertiary,
             value: 0
          },
          {
-            id: 18,
-            title: "1800 - Accumulated depreciation",
-            value: 0
-         },
-         {
-            id: "SumFixAsset",
-            title: "Total fixed assets",
-            type: ITEM_TYPES.TotalSecondary,
-            value: (items) => {
-               let val15 = items.find((x) => x.id == 15).value || 0;
-               let val16 = items.find((x) => x.id == 16).value || 0;
-               let val17 = items.find((x) => x.id == 17).value || 0;
-               let val18 = items.find((x) => x.id == 18).value || 0;
-
-               return val15 + val16 + val17 + val18;
-            }
-         },
-         {
-            title: "TOTAL ASSETS",
+            title: "Total Assets",
             type: ITEM_TYPES.Total,
             value: (items) => {
-               let valSumCurrentAsset =
-                  items.find((x) => x.id == "SumCurrentAsset").value(items) ||
-                  0;
-               let valSumFixAsset =
-                  items.find((x) => x.id == "SumFixAsset").value(items) || 0;
+               let val11 = items.find((x) => x.id == 11).value || 0;
+               let valSum12 =
+                  items.find((x) => x.id == "Sum12").value(items) || 0;
+               let val17 = items.find((x) => x.id == 17).value || 0;
 
-               return valSumCurrentAsset + valSumFixAsset;
+               return val11 + valSum12 + val17;
             }
          },
          {
-            title: "LIABILITIES AND FUND BALANCE",
-            type: ITEM_TYPES.Header
-         },
-         {
-            title: "Current liabilities",
+            title: "LIABILITIES",
             type: ITEM_TYPES.Header
          },
          {
             id: 21,
-            title: "2100 - Accounts payable",
+            title: "2100 - Payroll items payable",
             value: 0
          },
          {
             id: 22,
-            title: "2200 - Notes payable",
+            title: "2200 - Payables",
             value: 0
          },
          {
-            id: 23,
-            title: "2300 - Other current liabilities",
+            id: 26,
+            title: "2600 - Other Current Liabilities",
             value: 0
          },
          {
-            id: 25,
-            title: "2500 - Long-term liabilities",
+            id: 27,
+            title: "2700 - Long-term liabilities",
             value: 0
          },
          {
             id: "SumLiabilities",
-            title: "Total current liabilities",
+            title: "Total Current Liabilities",
             type: ITEM_TYPES.TotalSecondary,
             value: (items) => {
                let val21 = items.find((x) => x.id == 21).value || 0;
                let val22 = items.find((x) => x.id == 22).value || 0;
-               let val23 = items.find((x) => x.id == 23).value || 0;
-               let val25 = items.find((x) => x.id == 25).value || 0;
+               let val26 = items.find((x) => x.id == 26).value || 0;
+               let val27 = items.find((x) => x.id == 27).value || 0;
 
-               return val21 + val22 + val23 + val25;
+               return val21 + val22 + val26 + val27;
             }
          },
          {
-            title: "Fund Balance",
+            title: "FUND BALANCE",
             type: ITEM_TYPES.Header
          },
          {
-            id: 30,
-            title: "30 - Beginning fund balance",
+            id: 35,
+            title: "3500 - Beginning fund balance",
             value: 0
          },
          {
             id: 39,
-            title: "39 - Net income (loss) for the period",
+            title: "3900 - Net income (loss) year-to-date",
             value: 0
          },
          {
-            id: "SumFund",
+            id: "SumFundBalance",
             title: "Total Fund Balance",
             type: ITEM_TYPES.TotalSecondary,
             value: (items) => {
-               let val30 = items.find((x) => x.id == 30).value || 0;
+               let val35 = items.find((x) => x.id == 35).value || 0;
                let val39 = items.find((x) => x.id == 39).value || 0;
 
-               return val30 + val39;
+               return val35 + val39;
             }
          },
          {
-            title: "TOTAL LIABILITIES AND FUND BALANCE",
+            title: "Total Liabilities and Fund Balance",
             type: ITEM_TYPES.Total,
             value: (items) => {
                let valSumLiabilities =
                   items.find((x) => x.id == "SumLiabilities").value(items) || 0;
-               let valSumFund =
-                  items.find((x) => x.id == "SumFund").value(items) || 0;
+               let valSumFundBalance =
+                  items.find((x) => x.id == "SumFundBalance").value(items) || 0;
 
-               return valSumLiabilities + valSumFund;
+               return valSumLiabilities + valSumFundBalance;
             }
          }
       ]
    };
+}
+
+function GetRC(userData) {
+   const queryRC = ABSystemObject.getApplication().queries(
+      (o) => o.id == QUERY_IDS.RC
+   )[0];
+
+   if (queryRC == null) return Promise.resolve([]);
+
+   return new Promise((next, bad) => {
+      queryRC
+         .queryFind(
+            {
+               where: {
+                  glue: "and",
+                  rules: []
+               }
+            },
+            userData
+         )
+         .then((list) => {
+            let rcOptions = (list || []).map((rc) => rc["BASE_OBJECT.RC Name"]);
+
+            rcOptions = rcOptions.sort((a, b) =>
+               a.toLowerCase().localeCompare(b.toLowerCase())
+            );
+
+            next(rcOptions);
+         })
+         .catch(bad);
+   });
 }
 
 function GetFYMonths() {
@@ -276,7 +247,7 @@ function GetFYMonths() {
    });
 }
 
-function GetBalances(fyPeriod) {
+function GetBalances(rc, fyPeriod) {
    const objBalance = ABSystemObject.getApplication().objects(
       (o) => o.id == OBJECT_IDS.BALANCE
    )[0];
@@ -285,20 +256,33 @@ function GetBalances(fyPeriod) {
       return Promise.resolve([]);
    }
 
+   let cond = {
+      glue: "and",
+      rules: []
+   };
+
+   // TODO
+   if (rc && false) {
+      cond.rules.push({
+         key: "RC Code",
+         rule: "equals",
+         value: rc
+      });
+   }
+
+   if (fyPeriod) {
+      cond.rules.push({
+         key: "FY Period",
+         rule: "equals",
+         value: fyPeriod
+      });
+   }
+
    return new Promise((next, bad) => {
       objBalance
          .modelAPI()
          .findAll({
-            where: {
-               glue: "and",
-               rules: [
-                  {
-                     key: "549ab4ac-f436-461d-9777-505d6dc1d4f7",
-                     rule: "equals",
-                     value: fyPeriod
-                  }
-               ]
-            },
+            where: cond,
             populate: true
          })
          .then((list) => {
@@ -311,9 +295,21 @@ function GetBalances(fyPeriod) {
 module.exports = {
    // GET: /template/balanceSheet
    getData: (req, res) => {
-      let viewData = GetViewData(req.query.month);
+      let viewData = GetViewData(req.query.rc, req.query.month);
 
       Promise.resolve()
+         // Pull RC
+         .then(
+            () =>
+               new Promise((next, err) => {
+                  GetRC(req.user.data)
+                     .then((list) => {
+                        viewData.rcOptions = list;
+                        next();
+                     })
+                     .catch(err);
+               })
+         )
          // Pull FY month list
          .then(
             () =>
@@ -330,7 +326,7 @@ module.exports = {
          .then(
             () =>
                new Promise((next, err) => {
-                  GetBalances(viewData.fyPeriod)
+                  GetBalances(viewData.rc, viewData.fyPeriod)
                      .then((list) => {
                         (list || []).forEach((bl) => {
                            if (
